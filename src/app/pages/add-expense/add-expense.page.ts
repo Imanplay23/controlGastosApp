fe18779b98fa436ee4ff70f8f2fc900db3636d61
Imanplay23 <yandisanchez0321@gmail.com>
2024-10-20 05:15:33 -0400

    algunos estilos y algo mas de la logica

--- src/app/pages/add-expense/add-expense.page.ts
+++ src/app/pages/add-expense/add-expense.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Expense } from 'src/app/interfaces/expense';
 
 @Component({
   selector: 'app-add-expense',
@@ -8,15 +9,21 @@ import { Router } from '@angular/router';
 })
 export class AddExpensePage {
   description: string = '';
-  amount: number | null = null;
+  amount: number = 0;
 
   constructor(private router: Router) {}
 
   addExpense() {
-    const newExpense = {
+    const newExpense: Expense = {
       description: this.description,
       amount: this.amount
     };
+
+
+    if (!this.description || this.amount === null || this.amount <= 0) {
+      alert('Por favor, ingresa una descripción y un monto válido.');
+      return;
+    }
 
     let expenses = JSON.parse(localStorage.getItem('expenses') || '[]');
     expenses.push(newExpense);
