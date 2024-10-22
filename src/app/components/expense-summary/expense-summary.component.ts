5b5271cec02b5888f0d5c31ab73ac6074cd4ce2b
Imanplay23 <yandisanchez0321@gmail.com>
2024-10-22 13:47:58 -0400

    algunas mejoras de diseno

--- src/app/components/expense-summary/expense-summary.component.ts
+++ src/app/components/expense-summary/expense-summary.component.ts
@@ -7,25 +7,28 @@ import { ExpenseService } from 'src/app/services/expense.service';
   styleUrls: ['./expense-summary.component.scss'],
 })
 export class ExpenseSummaryComponent implements OnInit {
-  budget: number = 0;
-  availableBalance: number = 0;
   totalSpent: number = 0;
+  budget: any;
+  availableBalance: number = 0;
 
   constructor(private expenseService: ExpenseService) {}
 
   ngOnInit() {
-    this.expenseService.budget$.subscribe(budget => {
-      this.budget = budget;
-      this.updateAvailableBalance();
-    });
+   this.expenseService.budget$.subscribe(budget => {
+    this.budget = budget;
+    this.updateAvailableBalance();
+  });
 
-    this.expenseService.expenses$.subscribe(expenses => {
-      this.totalSpent = expenses.reduce((acc, exp) => acc + exp.amount, 0);
-      this.updateAvailableBalance();
-    });
-  }
+  this.expenseService.expenses$.subscribe(expenses => {
+    this.totalSpent = expenses.reduce((acc, exp) => acc + exp.amount, 0);
+    this.updateAvailableBalance();
+  });
+}
 
-  updateAvailableBalance() {
-    this.availableBalance = this.budget - this.totalSpent;
+updateAvailableBalance() {
+  this.availableBalance = this.budget - this.totalSpent;
+}
+  saveBudget() {
+    this.expenseService.setBudget(this.budget);
   }
 }
--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -3,23 +3,27 @@ import { BehaviorSubject } from 'rxjs';
 import { Expense } from '../interfaces/expense';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ExpenseService {
   private expenses: Expense[] = [];
   private expensesSubject = new BehaviorSubject<Expense[]>([]);
   expenses$ = this.expensesSubject.asObservable();
+  private totalSpentSubject = new BehaviorSubject<number>(0);
+  private budgetSubject = new BehaviorSubject<any>('');
+  private availableBalanceSubject = new BehaviorSubject<number>(0);
 
   private budget: number = 0;
-  private budgetSubject = new BehaviorSubject<number>(0);
+  totalSpent$ = this.totalSpentSubject.asObservable();
   budget$ = this.budgetSubject.asObservable();
+  availableBalance$ = this.availableBalanceSubject.asObservable();
 
   constructor() {
     this.loadExpenses();
     this.loadBudget();
   }
 
-  loadExpenses() {
+  private loadExpenses() {
     const storedExpenses = localStorage.getItem('expenses');
     if (storedExpenses) {
       this.expenses = JSON.parse(storedExpenses);
@@ -27,7 +31,7 @@ export class ExpenseService {
     }
   }
 
-  loadBudget() {
+  loadBudget(){
     const storedBudget = localStorage.getItem('budget');
     if (storedBudget) {
       this.budget = parseFloat(storedBudget);
@@ -35,22 +39,35 @@ export class ExpenseService {
     }
   }
 
-  setBudget(amount: number) {
-    this.budget = amount;
-    this.budgetSubject.next(this.budget);
-    localStorage.setItem('budget', amount.toString());
+  // private calculateTotals() {
+  //   const totalSpent = this.expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  //   const budget = this.budgetSubject.value;
+  //   const availableBalance = budget - totalSpent;
+
+  //   this.totalSpentSubject.next(totalSpent);
+  //   this.availableBalanceSubject.next(availableBalance);
+  // }
+
+  getExpenses() {
+    return this.expenses;
   }
 
-  addExpense(expense: Expense) {
+  addExpense(expense: any) {
     this.expenses.push(expense);
     this.expensesSubject.next(this.expenses);
     localStorage.setItem('expenses', JSON.stringify(this.expenses));
   }
 
-  removeExpense(id: string) {
+  deleteExpense(id: string) {
     this.expenses = this.expenses.filter(exp => exp.id !== id);
     this.expensesSubject.next(this.expenses);
     localStorage.setItem('expenses', JSON.stringify(this.expenses));
+  }
+
+  setBudget(amount: number) {
+    this.budget = amount;
+    this.budgetSubject.next(this.budget);
+    localStorage.setItem('budget', amount.toString());
   }
 
   getAvailableBalance(): number {
